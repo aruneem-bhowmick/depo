708279e51ebfd5b1ffab010a1f0fde0f5887d870
Aruneem Bhowmick <aruneem.bhowmick@gmail.com>
2026-05-25 19:45:01 -0500

    feat(session): add getSession() helper using next/headers

---
+++ lib/session.ts
@@ -0,0 +1,8 @@
+import { getIronSession } from 'iron-session'
+import { cookies } from 'next/headers'
+import { sessionOptions } from './sessionOptions'
+import type { SessionData } from './types'
+
+export async function getSession() {
+  return getIronSession<SessionData>(cookies(), sessionOptions)
+}
